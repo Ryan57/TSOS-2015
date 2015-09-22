ea085341335b52f5b37f5826cfe0f9fb35404159
Ryan57 <Ryan.Owens1@marist.edu>
2015-09-22 13:27:20 -0700

    Backspace implementation mk.1
    Started implementing backspace functionality by giving keyCode
    recognition of character code 8

--- source/os/deviceDriverKeyboard.ts
+++ source/os/deviceDriverKeyboard.ts
@@ -43,12 +43,19 @@ module TSOS {
                 }
                 // TODO: Check for caps-lock and handle as shifted if so.
                 _KernelInputQueue.enqueue(chr);
+
             } else if (((keyCode >= 48) && (keyCode <= 57)) ||   // digits
                         (keyCode == 32)                     ||   // space
                         (keyCode == 13)) {                       // enter
                 chr = String.fromCharCode(keyCode);
                 _KernelInputQueue.enqueue(chr);
+
+            } else if (keyCode == 8)                           //backspace
+            {
+                chr = String.fromCharCode (keyCode);
+                _KernelInputQueue.enqueue(chr);
             }
         }
+
     }
 }
