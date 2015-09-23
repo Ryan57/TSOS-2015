ba8819985e38896087d94ce4a063a95b103c6256
Ryan57 <Ryan.Owens1@marist.edu>
2015-09-22 17:14:16 -0700

    BSOD mk.1
    Attempt to display a newly colored screen displaying an error message to
    the user

--- source/os/kernel.ts
+++ source/os/kernel.ts
@@ -174,6 +174,10 @@ module TSOS {
         public krnTrapError(msg) {
             Control.hostLog("OS ERROR - TRAP: " + msg);
             // TODO: Display error on console, perhaps in some sort of colored screen. (Maybe blue?)
+
+            _DrawingContext.fillStyle("blue");
+            _DrawingContext.fillRect(0, 0, _Canvas.width, _Canvas.height);
+
             this.krnShutdown();
         }
     }
